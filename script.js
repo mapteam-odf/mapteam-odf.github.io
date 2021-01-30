2eb75799b834f9a845757db688c863133e7999f6
trybuildtogether <buildtogether.team@gmail.com>
2021-01-30 14:09:15 -0800

    pin

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: { lat: 122.5076399, lng: 37.7576793 },
+    center: { lat: 37.7576793, lng: 122.5076399 },
   });
   const image =
     "https://mapteam-odf.github.io/circle-map.png";
   const beachMarker = new google.maps.Marker({
-    position: { lat: -33.89, lng: 151.274 },
+    position: { lat: 37.7576793, lng: 122.5076399 },
     map,
     icon: image,
   });

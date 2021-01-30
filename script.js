ea850f83929f6d6648fb49423c868c404a343a49
trybuildtogether <buildtogether.team@gmail.com>
2021-01-30 14:20:41 -0800

    location now roking

--- script.js
+++ script.js
@@ -3,13 +3,31 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: { lat: 122.5076399, lng: 37.7576793 },
+    center: { lat: 37.75, lng: 122.50 },
   });
   const image =
     "https://mapteam-odf.github.io/circle-map.png";
   const beachMarker = new google.maps.Marker({
-    position: { lat: 122.5076399, lng: 37.7576793 },
+    position: { lat: 37.75, lng: 122.50 },
     map,
     icon: image,
   });
+
+  function displayMarkers (evt) {
+    var message;
+    if (evt.origin !== "https://community.beondeck.com/" || evt.origin !== "https://mapteam-odf.github.io//" || ) {
+      alert("Not gonna happen");
+    }
+    else {
+      console.log(evt.data);
+    }
+  }
+
+  if (window.addEventListener) {
+    // For standards-compliant web browsers
+    window.addEventListener("message", displayMarkers, false);
+  }
+  else {
+    window.attachEvent("onmessage", displayMarkers);
+  }
 }
